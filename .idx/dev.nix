84e55270ef42d7a179da21e85337fb2ac9e58057
aadilayub15 <aadilayub15@gmail.com>
2024-10-24 17:18:59 +0000

    add idx config

---
+++ .idx/dev.nix
@@ -0,0 +1,24 @@
+{pkgs}: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodejs_20
+  ];
+
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}

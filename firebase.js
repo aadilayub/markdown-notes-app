3fd531521aafdaf13c3f9bd868b2e00b470f4d81
aadilayub15 <aadilayub15@gmail.com>
2024-10-24 17:49:07 +0000

    atore notes in firestore collection

--- firebase.js
+++ firebase.js
@@ -13,4 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
-const notesCollection = collection(db)
+export const notesCollection = collection(db, 'notes')

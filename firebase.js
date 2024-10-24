11da6f8886be9c31967f354088515af08b419497
aadilayub15 <aadilayub15@gmail.com>
2024-10-24 17:27:10 +0000

    formatting

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app"
 import { getFirestore, collection } from "firebase/firestore"
 
 const firebaseConfig = {
-	apiKey: "AIzaSyA1ZCHh1L4LzyQr79QWb0kRODRlRsRsV7Q",
-	authDomain: "react-notes-5b45d.firebaseapp.com",
-	projectId: "react-notes-5b45d",
-	storageBucket: "react-notes-5b45d.appspot.com",
-	messagingSenderId: "674530494669",
-	appId: "1:674530494669:web:7952d2643d5583029d8184"
+    apiKey: "AIzaSyA1ZCHh1L4LzyQr79QWb0kRODRlRsRsV7Q",
+    authDomain: "react-notes-5b45d.firebaseapp.com",
+    projectId: "react-notes-5b45d",
+    storageBucket: "react-notes-5b45d.appspot.com",
+    messagingSenderId: "674530494669",
+    appId: "1:674530494669:web:7952d2643d5583029d8184"
 }
 
 
